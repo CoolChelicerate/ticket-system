73b5669d68852af3bee82da49e72518db34de5fd
CoolChelicerate <jamesmcgraith@aol.com>
2026-04-22 21:11:18 -0700

    Small changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,14 +13,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full antialiased">
       <head>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
-      <body>
+      <body className="min-h-full">
         <Script
           src="https://kit.fontawesome.com/32c2532505.js"
           crossOrigin="anonymous"
